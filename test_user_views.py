49bec61ac617d9241be0e54c1c302315449e5970
Chris <chris.conner57@gmail.com>
2021-01-23 16:54:11 -0800

    Added more test

--- test_user_views.py
+++ test_user_views.py
@@ -4,7 +4,7 @@ from flask import session
 from models import db, User
 import os
 
-os.environ['DATABASE_URL'] = "postgresql:///warbler-test"
+os.environ['DATABASE_URL'] = "postgresql:///covid-19-test"
 
 db.create_all()
 
@@ -29,4 +29,23 @@ class FlaskTests(TestCase):
         db.session.commit()
 
         self.client = app.test_client()
-        app.config['TESTING'] = True
+
+    def tearDown(self):
+        User.query.delete()
+
+        db.session.commit()
+
+    def test_repr(self):
+        u = User.query.filter_by(username='testuser').one()
+
+        self.assertEqual(repr(u), f'<User #{u.id}: {u.email}, {u.password}>')
+
+    def test_signup(self):
+        self.assertTrue(User.signup('test3@test.com', 'testpassword'))
+        db.session.rollback()
+        self.assertTrue(User.signup('test3@test.com', 'testpassword'))
+
+    def test_authenticate(self):
+        u1 = User.query.filter_by(username='testuser').one()
+
+        self.assertFalse(User.authenticate('fakeuser', 'HASHED_PASSWORD'))
